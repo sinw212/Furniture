cda7dfbdbe95004b971aed3f80d21088fadcd207
sainkr <rrlooa1090@naver.com>
2019-10-04 15:11:58 +0900

    cam1-1

---
+++ VRFA/app/src/main/java/com/example/vrfa/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.vrfa;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static CameraPreview surfaceView;
+    private SurfaceHolder holder;
+    private static Button camera_preview_button;
+    private static Camera mCamera;
+    private int RESULT_PERMISSIONS = 100;
+    public static MainActivity getInstance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 카메라 프리뷰를  전체화면으로 보여주기 위해 셋팅한다.
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        // 안드로이드 6.0 이상 버전에서는 CAMERA 권한 허가를 요청한다.
+        requestPermissionCamera();
+    }
+    public static Camera getCamera(){
+        return mCamera;
+    }
+    private void setInit(){
+        getInstance = this;
+
+        // 카메라 객체를 R.layout.activity_main의 레이아웃에 선언한 SurfaceView에서 먼저 정의해야 함으로 setContentView 보다 먼저 정의한다.
+        mCamera = Camera.open();
+
+        setContentView(R.layout.activity_main);
+
+        // SurfaceView를 상속받은 레이아웃을 정의한다.
+        surfaceView = (CameraPreview) findViewById(R.id.preview);
+
+
+        // SurfaceView 정의 - holder와 Callback을 정의한다.
+        holder = surfaceView.getHolder();
+        holder.addCallback(surfaceView);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public boolean requestPermissionCamera(){
+        int sdkVersion = Build.VERSION.SDK_INT;
+        if(sdkVersion >= Build.VERSION_CODES.M) {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        RESULT_PERMISSIONS);
+
+            }else {
+                setInit();
+            }
+        }else{  // version 6 이하일때
+            setInit();
+            return true;
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        if (RESULT_PERMISSIONS == requestCode) {
+
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한 허가시
+                setInit();
+            } else {
+                // 권한 거부시
+            }
+            return;
+        }
+
+    }
+}
